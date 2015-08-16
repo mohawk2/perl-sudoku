d954f43edf79c0808f914253a0f44cabc5bca5aa
Ed J <mohawk2@users.noreply.github.com>
2015-08-16 23:34:19 +0100

    Class Sudoku::Set

--- sudokusolver.pl
+++ sudokusolver.pl
@@ -2,9 +2,6 @@
 
 use strict;
 
-#use Data::Dumper; print Data::Dumper::Dumper([ Sudoko::Bigsquare::init2nonblank(@ESCARGOT) ]);
-#use Data::Dumper; print Data::Dumper::Dumper([ Sudoko::Bigsquare::related_xys(4, 6) ]);
-
 die "usage: $0 <inputfile>\n" unless @ARGV;
 my @input = split ' ', read_file($ARGV[0]);
 
@@ -15,9 +12,10 @@ print_bigsquare($bigsquare);
 while (my @solved = $bigsquare->get_solved) {
  print '.';
  map { $bigsquare->setvalue(@$_) } @solved;
+#die if $::count++ > 2;
 }
 print "---\n";
-#use Data::Dumper; print Data::Dumper::Dumper([ $bigsquare->allvalues ]);#$bigsquare->{xy2possvalue21});#
+#use Data::Dumper; print Data::Dumper::Dumper($bigsquare->{settype_id2set});
 print_bigsquare($bigsquare);
 
 sub print_bigsquare {
@@ -33,20 +31,71 @@ sub read_file {
  <FH>;
 }
 
-package Sudoko::Bigsquare;
+package Sudoku::Set;
 
-# a bigsquare has 3 types of sets: x, y, square
-# within square is each row left-right, top row first
-# square offset is same
-# all offsets and xy coords start from 0
-
-# internal data structures: xy2possvalue21 (i.e. the value is 1, so do "keys" on it to get poss nums)
-#  when the xy is setvalue'd, the key is deleted
+# has 9 locations, each of which can have 1 value, which is 1-9, which can only occur once each
 
 sub new {
  my ($class) = @_;
  my $self = {};
- $self->{sets} = [];
+ map {
+  my $value = $_;
+  map {
+   my $loc = $_;
+   $self->{value2loc21}->{$value}->{$loc} = 1;
+  } (0..8);
+ } (1..9);
+ bless $self, $class;
+}
+
+# if $loc undef, remove all that value's possible locations
+# if $value undef, remove all that location's possible values
+sub remove_poss {
+ my ($self, $value, $loc) = @_;
+ if (not defined $value) {
+  die "must define either value or loc\n" unless defined $loc;
+  map {
+   my $value = $_;
+   delete $self->{value2loc21}->{$value}->{$loc} if exists $self->{value2loc21}->{$value}; # no auto-viv
+  } (1..9);
+  return;
+ }
+ return unless exists $self->{value2loc21}->{$value};
+ if (not defined $loc) {
+  delete $self->{value2loc21}->{$value};
+  return;
+ }
+ delete $self->{value2loc21}->{$value}->{$loc};
+}
+
+# returns list of [ loc, value ]
+sub get_solved {
+ my $self = shift;
+ my @loc_vals;
+ map {
+  my $value = $_;
+  my @locs = keys %{ $self->{value2loc21}->{$value} };
+  if (@locs == 1) {
+   push @loc_vals, [ $locs[0], $value ];
+  }
+ } keys %{ $self->{value2loc21} };
+ @loc_vals;
+}
+
+package Sudoko::Bigsquare;
+
+# a bigsquare has 3 types of sets: row, col, square
+# within square is each row left-right, top row first
+# square offset is same
+# all offsets and xy coords start from 0
+
+# internal data structures:
+# settype_id2value2loc21: "$type$id" -> value -> loc -> 1 (as above, keys to get possibilities)
+
+sub new {
+ my ($class) = @_;
+ my $self = {};
+ $self->{settype_id2set} = {};
  $self->{y2x2value} = [];
  my @all_xys = map {
   my $y = $_;
@@ -57,21 +106,22 @@ sub new {
  map {
   my $this_xy = $_;
   $self->{y2x2value}->[$this_xy->[1]]->[$this_xy->[0]] = 0;
-  map { $self->{xy2possvalue21}->{join '', @$this_xy}->{$_} = 1; } (1..9);
  } @all_xys;
-#use Data::Dumper; print Data::Dumper::Dumper($self->{xy2possvalue21});die;#[ $bigsquare->get_solved ]);
+ map {
+  my $settype = $_;
+  map {
+   my $set = $_;
+   $self->{settype_id2set}->{$settype.$set} = Sudoku::Set->new;
+  } (0..8);
+ } (0..2);
  bless $self, $class;
 }
 
 sub setvalue {
  my ($self, $x, $y, $value) = @_;
  $self->{y2x2value}->[$y]->[$x] = $value;
-#warn "$self ($x, $y, $value)\n";
+#warn "$self->setvalue($x, $y, $value)\n";
 #use Data::Dumper; print Data::Dumper::Dumper([ xy2type_set_offset($x, $y) ]);
- map {
-  my ($type, $set, $offset) = @$_;
-  $self->{sets}->[$type]->[$set]->[$offset] = $value;
- } xy2type_set_offset($x, $y);
  $self->remove_poss($x, $y);
  map { $self->remove_poss(@$_, $value); } related_xys($x, $y);
 }
@@ -79,19 +129,30 @@ sub setvalue {
 # if $value not defined, delete all possibilities
 sub remove_poss {
  my ($self, $x, $y, $value) = @_;
-#warn "removing ($x, $y, $value)\n";
- if (defined $value) {
-  delete $self->{xy2possvalue21}->{"$x$y"}->{$value} if exists $self->{xy2possvalue21}->{"$x$y"};
- } else {
-  delete $self->{xy2possvalue21}->{"$x$y"};
- }
+#warn "removing ($x, $y, @{[ defined($value) ? $value : 'undef' ]})\n";
+ map {
+  my ($type, $set, $offset) = @$_;
+  $self->{settype_id2set}->{$type.$set}->remove_poss($value, $offset);
+ } xy2type_set_offset($x, $y);
 }
 
 # output list of [ x, y, value ]
 sub get_solved {
  my $self = shift;
- my $xy2pn = $self->{xy2possvalue21};
- map { [ (split //, $_), (keys %{ $xy2pn->{$_} })[0] ] } grep { keys %{ $xy2pn->{$_} } == 1 } keys %$xy2pn;
+ my %xy2v;
+ my $si2set = $self->{settype_id2set};
+ map {
+  my $si = $_;
+  my $set = $si2set->{$si};
+  map {
+   my ($loc, $value) = @$_;
+   my ($settype, $setid) = split //, $si;
+   my ($x, $y) = type_set_offset2xy($settype, $setid, $loc);
+#warn "GOT SOLVED: $loc, $value = ($settype, $setid) = $x, $y\n";
+   $xy2v{$x.$y} = $value;
+  } $set->get_solved;
+ } keys %$si2set;
+ map { [ (split //, $_), $xy2v{$_} ] } keys %xy2v;
 }
 
 # output list of rows
@@ -117,23 +178,34 @@ sub related_xys {
  );
 }
 
-# output offset number of square
-sub xy2square {
- my ($x, $y) = @_;
- int($x / 3) + 3 * int($y / 3);
-}
-
 # input (x, y)
 # output 3 x [ type, set, offset ]
 sub xy2type_set_offset {
  my ($x, $y) = @_;
- my $whichsquare = xy2square($x, $y);
+ my $whichsquare =  int($x / 3) + 3 * int($y / 3);
  my $square_offset = ($x % 3) + 3 * ($y % 3);
  (
   [ 0, $y, $x ],
   [ 1, $x, $y ],
   [ 2, $whichsquare, $square_offset ],
  );
+}
+
+# input (type, set, offset)
+# output (x, y)
+sub type_set_offset2xy {
+ my ($type, $setid, $offset) = @_;
+ if ($type == 0) {
+  return ($offset, $setid);
+ } elsif ($type == 1) {
+  return ($setid, $offset);
+ } elsif ($type == 2) {
+  my $x_left_square = 3 * ($setid % 3);
+  my $y_top_square = 3 * int($setid / 3);
+  my $x_offset = $offset % 3;
+  my $y_offset = int($offset / 3);
+  return ($x_left_square + $x_offset, $y_top_square + $y_offset);
+ }
 }
 
 # input is list of 27 3-char strings
